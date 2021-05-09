37a19464cbac1b9e74650eb821f228d02926e5f4
hgxey6 <hgxey6@gmail.com>
2021-05-09 22:55:46 +0500

    add initial business logic for the user

---
+++ school-app/src/main/java/edu/education/schoolapp/domain/User.java
@@ -0,0 +1,28 @@
+package edu.education.schoolapp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ school-app/src/main/java/edu/education/schoolapp/service/UserService.java
@@ -0,0 +1,51 @@
+package edu.education.schoolapp.service;
+
+import edu.education.schoolapp.domain.User;
+import edu.education.schoolapp.exception.UserNotFoundException;
+import edu.education.schoolapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(Long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new UserNotFoundException(id));
+    }
+
+    public User updateUser(User newUser, Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setPassword(newUser.getPassword());
+                    return userRepository.save(user);
+                })
+                .orElseGet(() -> {
+                    newUser.setId(id);
+                    return userRepository.save(newUser);
+                });
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
